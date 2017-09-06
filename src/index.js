2623c2e6a3533a75ef6eff7c7687ba09e7b0b95d
Hannu Kärkkäinen <hannu.karkkainen@gmail.com>
2017-09-06 21:23:22 +0900

    Working prototype with 2 range inputs

--- src/index.js
+++ src/index.js
@@ -1,22 +1,57 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { Component } from 'react';
 
-const handleChange = (event, type, cb) => {
-  let value = event.target.value;
-  const isInt = v => /^-?[0-9]+$/.test(`${v}`);
-  value = isInt(value) ? parseInt(value, 10) : parseFloat(value);
-  cb(value);
-};
+class Slider extends Component {
+  static propTypes = {
+    label: PropTypes.string,
+    min: PropTypes.number,
+    max: PropTypes.number,
+    step: PropTypes.number,
+    defaultValue: PropTypes.number,
+    onChange: PropTypes.func.isRequired
+  };
+  static defaultProps = {
+    min: 0,
+    max: 10,
+    step: 0.1,
+    defaultValue: 0
+  }
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: props.defaultValue,
+      miniValue: 0
+    }
+  }
+  handleMiniChange(event) {
+    const { value, miniValue } = this.state;
+    const nextMini = parseFloat(event.target.value);
+    const nextValue = (value - miniValue) + nextMini
+    this.setState({
+      value: nextValue,
+      miniValue: nextMini
+    });
+    this.props.onChange(nextValue);
+  }
+  handleChange(event) {
+    let value = parseFloat(event.target.value);
+    this.setState({ value });
+    this.props.onChange(value);
+  };
 
-const Slider = props => {
-  const { onChange, ...rest } = props;
-  return (
-    <input {...rest} onChange={e => handleChange(e, props.type, props.onChange)} />
-  )
-};
-
-Slider.propTypes = {
-  onChange: PropTypes.func
-};
-
+  render() {
+    const { value, miniValue } = this.state;
+    const { defaultValue, label, step, onChange, ...rest } = this.props;
+    return (
+      <div>
+        <label>{label}</label>
+        <header>{value}</header>
+        <div>
+          <input type="range" min={-step} max={step} step={step/100} value={miniValue} onChange={e => this.handleMiniChange(e)} />
+          <input type="range" step={step} {...rest} value={value} onChange={e => this.handleChange(e)} />
+        </div>
+      </div>
+    )
+  }
+}
 export default Slider;
