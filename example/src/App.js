c4a84d13bdae644ef4b422f764f70d462c0be7df
Hannu Kärkkäinen <hannu.karkkainen@gmail.com>
2017-09-11 19:37:21 +0900

    Updated example

--- example/src/App.js
+++ example/src/App.js
@@ -11,9 +11,21 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>react-precision-slider</h2>
         </div>
-        <div className="App-content">
+        <div className="big-slider">
+          <h3>Wide slider</h3>
           <Slider
             label="Scale"
+            min={0}
+            max={10}
+            step={0.01}
+            defaultValue={1.0}
+            onChange={() => {}}
+          />
+        </div>
+        <div className="small-slider">
+          <h3>Narrow slider</h3>
+          <Slider
+            label="Power"
             min={0}
             max={10}
             step={0.01}
